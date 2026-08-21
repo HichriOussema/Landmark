e702a54aada41273b49b3598e3c8d5b285ad605a
HichriOussema <oussema.mouhib.hichri@gmail.com>
2026-08-21 18:30:04 +0100

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Oussema mouhib HICHRI on 21/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
